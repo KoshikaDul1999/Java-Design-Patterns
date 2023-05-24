121cc8651dcda161034fad19944d43bcf8d3f474
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-25 00:04:09 +0530

    updated main class

--- SingleronDesignPattern/src/com/koshika/designpatterns/SingletonDemo.java
+++ SingleronDesignPattern/src/com/koshika/designpatterns/SingletonDemo.java
@@ -2,6 +2,9 @@ package com.koshika.designpatterns;
 
 public class SingletonDemo {
     public static void main(String[] args) {
+        //Testing the Singleton variation
 
+        EagerSingleton eagerSingleton = EagerSingleton.getInstance();
+        eagerSingleton.showMessage();
     }
 }
