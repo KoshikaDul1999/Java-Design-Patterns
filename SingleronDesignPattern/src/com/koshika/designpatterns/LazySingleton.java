75272abc8f6077a04f5c75f339866e367834bf85
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-25 00:07:52 +0530

    Create LazySingleton.java

---
+++ SingleronDesignPattern/src/com/koshika/designpatterns/LazySingleton.java
@@ -0,0 +1,24 @@
+package com.koshika.designpatterns;
+
+public class LazySingleton {
+
+    private static LazySingleton lazySingleton;
+
+
+    private LazySingleton(){
+    }
+
+
+    public static LazySingleton getInstance(){
+        if (lazySingleton == null)
+        {
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+
+
+    public void showMessage(){
+        System.out.println("Hello form Lazy Singleton...");
+    }
+}
