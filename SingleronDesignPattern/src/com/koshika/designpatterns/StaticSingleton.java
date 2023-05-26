f1921b5908da8e67aa54bd0ff9d4fc95f6b6c505
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-26 21:50:11 +0530

    Create StaticSingleton.java

---
+++ SingleronDesignPattern/src/com/koshika/designpatterns/StaticSingleton.java
@@ -0,0 +1,32 @@
+package com.koshika.designpatterns;
+
+public class StaticSingleton {
+
+    //creating a private static instance
+    private static StaticSingleton staticSingleton;
+
+    //Make contructor private
+    private StaticSingleton(){
+    }
+
+    //Static block initialization for exception handling
+    static {
+        try{
+            staticSingleton = new StaticSingleton();
+        }catch (Exception e){
+            throw new RuntimeException("Exception occured while creating the object");
+        }
+    }
+
+    //Return the only object available
+    public  static StaticSingleton getInstance(){
+        return staticSingleton;
+    }
+
+    //Sample method to test
+    public void showMessage(){
+        System.out.println("Hello From Static Singleton...!!!");
+    }
+
+
+}
