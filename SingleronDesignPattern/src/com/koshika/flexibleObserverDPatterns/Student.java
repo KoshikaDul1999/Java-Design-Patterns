bb5d331f0ca5508fc85903bda5c99c78281b7249
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-09 04:54:22 +0530

    Update Student.java

--- SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Student.java
+++ SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Student.java
@@ -1,4 +1,15 @@
 package com.koshika.flexibleObserverDPatterns;
 
-public class Student {
+public class Student implements StudentObserver{
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println("I.m " + this.name + " I got the message " + message);
+    }
 }
