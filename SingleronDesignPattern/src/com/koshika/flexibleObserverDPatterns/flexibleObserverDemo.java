f8954c03e2c5b002360cec49a0d9905684ec5e9f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-09 05:04:30 +0530

    Update flexibleObserverDemo.java

--- SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/flexibleObserverDemo.java
+++ SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/flexibleObserverDemo.java
@@ -2,6 +2,10 @@ package com.koshika.flexibleObserverDPatterns;
 
 public class flexibleObserverDemo {
     public static void main(String[] args) {
+        Admin admin = new Admin();
+        Student student = new Student("Koshika");
 
+        admin.addOberver(student);
+        admin.notifyObserver();
     }
 }
