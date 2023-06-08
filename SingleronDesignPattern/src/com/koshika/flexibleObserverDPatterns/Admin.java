0dc920b177631f365f7878399cfda1111ac5de6d
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-09 04:45:08 +0530

    Update Admin.java

--- SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Admin.java
+++ SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Admin.java
@@ -1,4 +1,18 @@
 package com.koshika.flexibleObserverDPatterns;
 
-public class Admin {
+public class Admin implements AdminObserverble{
+    @Override
+    public void addOberver(StudentObserver studentObserver) {
+
+    }
+
+    @Override
+    public void removeObserver(StudentObserver studentObserver) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
 }
