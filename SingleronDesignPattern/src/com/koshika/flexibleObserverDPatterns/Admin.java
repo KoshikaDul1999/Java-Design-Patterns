ff8c5f1ceee37f24f527d626ec18cb9dab93c00a
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-09 04:51:38 +0530

    Update Admin.java

--- SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Admin.java
+++ SingleronDesignPattern/src/com/koshika/flexibleObserverDPatterns/Admin.java
@@ -1,12 +1,15 @@
 package com.koshika.flexibleObserverDPatterns;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Admin implements AdminObserverble{
 
     ArrayList<StudentObserver> studentObserverArrayList = new ArrayList<>();
 
     private String msg;
+
+    Scanner scanner = new Scanner(System.in);
     @Override
     public void addOberver(StudentObserver studentObserver) {
         studentObserverArrayList.add(studentObserver);
@@ -22,5 +25,11 @@ public class Admin implements AdminObserverble{
         for(StudentObserver studentObserver : studentObserverArrayList){
             studentObserver.update(msg);
         }
+    }
+
+    public void sendMessage(){
+        System.out.println("Enter the message :");
+        msg = scanner.nextLine();
+        notifyObserver();
     }
 }
